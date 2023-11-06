f7c5e6299e3c31ea1ae1f659134a58d5ff7bbb01
MeanBordin <bodinsak2544@gmail.com>
2023-11-06 23:54:45 +0700

    up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'admin', pathMatch: 'full'},
   {
     path: 'admin',
     loadChildren: () =>
       import('./admin/admin.module').then((m) => m.AdminModule),
   },
+  { path: '**', redirectTo: 'admin' },
 ];
 
 @NgModule({
