2c6ee090c1b3202de1def7b807112a9d2f277230
MeanBordin <bodinsak2544@gmail.com>
2023-11-06 23:49:07 +0700

    up

--- src/app/admin/constants/table-option.ts
+++ src/app/admin/constants/table-option.ts
@@ -3,8 +3,8 @@ export const TABLE_SEARCH = [
     { field: 'id', header: 'No.' },
     { field: 'image', header: 'Image' },
     { field: 'name', header: 'Productname' },
-    { field: 'price', header: 'Price' },
     { field: 'stock', header: 'Stock' },  
+    { field: 'price', header: 'Price' },
     { field: 'createdAt', header: 'Created'},
     { field: 'updatedAt', header: 'Updated' },
 ]
